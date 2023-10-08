17d615d88b2fcac0dba2e508f8b3ad13ca141a5e
fchimpan <ryo-mimura@cybozu.co.jp>
2023-10-08 17:58:54 +0900

    feat: ent shcema

--- golang/app/ent/schema/user.go
+++ golang/app/ent/schema/user.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // User holds the schema definition for the User entity.
 type User struct {
@@ -9,10 +15,19 @@ type User struct {
 
 // Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("name"),
+		field.Int("age"),
+		field.String("nickname").Unique(),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the User.
 func (User) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("pets", Pet.Type),
+		edge.From("groups", Group.Type).Ref("users"),
+	}
 }
--- golang/app/main.go
+++ golang/app/main.go
@@ -1,28 +1,24 @@
-package app
+package main
 
 import (
-	"fmt"
+	"context"
 	"log"
-	"os"
+	"server/middleware"
 
-	"ariga.io/sqlcomment/examples/ent"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	dbUser := os.Getenv("MYSQL_USER")
-	dbPass := os.Getenv("MYSQL_PASSWORD")
-	dbPort := os.Getenv("MYSQL_PORT")
-	dbName := os.Getenv("MYSQL_DATABASE")
-	if dbUser == "" || dbPass == "" || dbPort == "" || dbName == "" {
-		log.Fatal("DB Config Error")
-	}
-
-	client, err := ent.Open("mysql", fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?parseTime=True", dbUser, dbPass, dbPort, dbName))
+	entClient, err := middleware.NewEntClient()
 	if err != nil {
 		log.Fatalf("failed opening connection to mysql: %v", err)
 	}
-	defer client.Close()
+	defer entClient.Close()
+	log.Println("DB Connected!")
 
-	log.Println("DB Connected")
+	if err := entClient.Migrate(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	log.Println("DB Schema Created!")
+
 }
